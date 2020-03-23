70dbbb5d674d6b492a841ce79884b99f8641e7f1
伪淑女 <5642012+fake_lady@user.noreply.gitee.com>
2020-03-23 14:33:43 +0800

    static

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,20 @@ import Proptypes from 'prop-types';
 class Header extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            sonmsg:'header----sonmsg'
-         };
+        this.state = {
+            sonmsg: 'header----sonmsg'
+        };
     }
-    getFathMsg=()=>{
+    static defaultProps = {
+        title: '标题'
+    }
+    static propTypes = {
+        num: Proptypes.number
+    }
+    getFathMsg = () => {
         alert(this.props.father.state.title);
     }
-    run=()=>{
+    run = () => {
         alert('我是子组件的run方法');
     }
     render() {
@@ -21,19 +27,20 @@ class Header extends React.Component {
                 <button onClick={this.props.run}>调用父组件中的run方法</button>
                 <button onClick={this.props.father.getDate}>调用父组件中的getDate方法</button>
                 <button onClick={this.getFathMsg}>获取父组件中state中的title属性</button>
-                <button onClick={this.props.father.getSonData.bind(this,'我是子组件传过来的')}>子组件给父组件传值</button>
-                <button onClick={this.props.getSonData.bind(this,'我是子组件传过来的222')}>子组件给父组件传值2</button>
+                <button onClick={this.props.father.getSonData.bind(this, '我是子组件传过来的')}>子组件给父组件传值</button>
+                <button onClick={this.props.getSonData.bind(this, '我是子组件传过来的222')}>子组件给父组件传值2</button>
             </div>
         );
     }
 }
 
-//defaultProps：如果父组件调用子组件时，不给子组件传title属性，那么就在子组件中使用defaultProps定义的默认值
-Header.defaultProps={
-    title:'标题'
-}
-// propTypes:验证父组件传值类型的合法性,Proptypes.number中的Proptypes名称可以自己定义
-Header.propTypes={
-    num:Proptypes.number
-}
+//下面的定义方式可以变成上面 static 对象
+// // defaultProps：如果父组件调用子组件时，不给子组件传title属性，那么就在子组件中使用defaultProps定义的默认值
+// Header.defaultProps = {
+//     title: '标题'
+// }
+// // propTypes:验证父组件传值类型的合法性,Proptypes.number中的Proptypes名称可以自己定义
+// Header.propTypes = {
+//     num: Proptypes.number
+// }
 export default Header;
