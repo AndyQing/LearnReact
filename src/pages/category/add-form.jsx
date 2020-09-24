6f9289560c45252ecc3d9cf37f7a733c9d9f0be9
伪淑女 <5642012+fake_lady@user.noreply.gitee.com>
2020-09-24 14:06:26 +0800

    输入的金额

--- src/pages/category/add-form.jsx
+++ src/pages/category/add-form.jsx
@@ -4,6 +4,7 @@ import {
   Form,
   Select,
   Input,
+  InputNumber,
 } from 'antd'
 
 const Item = Form.Item
@@ -11,6 +12,13 @@ const Option = Select.Option
 /*
 添加分类的form组件
  */
+const limitDecimalsF = (value) => {
+  let reg = /^(-)*(\d+)\.(\d\d).*$/; return `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',').replace(reg, '$1$2.$3');
+};
+const limitDecimalsP = (value) => {
+  let reg = /^(-)*(\d+)\.(\d\d).*$/;
+  return value.replace(/￥\s?|(,*)/g, '').replace(reg, '$1$2.$3');
+};
 class AddForm extends Component {
   formRef = React.createRef();
   static propTypes = {
@@ -25,17 +33,13 @@ class AddForm extends Component {
   componentDidMount() {
     this.props.setForm(this.formRef.current)//该方法不能写在componentWillMount中
   }
-  onFinish = values => {
-    console.log("onFinish---", values);
-  };
-
   render() {
 
     const { categorys, parentId } = this.props
     // const { getFieldDecorator } = this.props.form
 
     return (
-      <Form onFinish={this.onFinish} ref={this.formRef}>
+      <Form ref={this.formRef}>
         <Item name='parentId' initialValue={parentId}>
           {/* {
             getFieldDecorator('parentId', {
@@ -67,6 +71,22 @@ class AddForm extends Component {
           <Input placeholder='请输入分类名称' />
           {/* )
           } */}
+        </Item>
+        {/* 个人测试，输入的金额会有逗号分隔 */}
+        <Item name='price'>
+          <InputNumber
+            style={{ width: 180 }}
+            min={0}
+            maxLength={13}
+
+            // precision={2}//precision加了之后，还是会可输入超过2位的，但是失去焦点后会四舍五入
+            // formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            // parser={value => value.replace(/\$\s?|(,*)/g, '')}
+            
+            //用下面的方法即可实现逗号分隔，又能只限制2位小数的输入
+            formatter={limitDecimalsF}
+            parser={limitDecimalsP}
+            placeholder='请输入预算' />
         </Item>
       </Form>
     )
