e402d3906172c717cc3128dbccd4c7c628327f30
伪淑女 <5642012+fake_lady@user.noreply.gitee.com>
2021-08-29 16:13:09 +0800

    btn加loadding

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import { Button } from 'antd'
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loadings: true,
+        };
     }
+    enterLoading = index => {
+        console.log('-------')
+        this.setState({ loadings: true })
+
+        // this.setState(({ loadings }) => {
+        //     const newLoadings = [...loadings];
+        //     newLoadings[index] = true;
+
+        //     return {
+        //         loadings: newLoadings,
+        //     };
+        // });
+        setTimeout(() => {
+            this.setState({ loadings: false })
+            // this.setState(({ loadings }) => {
+            //     const newLoadings = [...loadings];
+            //     newLoadings[index] = false;
+
+            //     return {
+            //         loadings: newLoadings,
+            //     };
+            // });
+        }, 6000);
+    };
     render() {
+        const { loadings } = this.state;
         return (
-            <div>home</div>
+            <div>home
+                <Button type="primary" loading={loadings}
+                    onClick={() => this.enterLoading(0)}>
+                    Click me!
+        </Button>
+            </div>
         );
     }
 }
